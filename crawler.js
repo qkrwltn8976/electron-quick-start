147cccd3c487264ed823ba066ceaefadb1abd87f
qkrwltn8976 <qkrwltn8976@naver.com>
2020-05-05 02:20:54 +0900

    Update crawler.js
    settimeout error handling

--- crawler.js
+++ crawler.js
@@ -2,30 +2,43 @@ let request = require('request');
 let cheerio = require('cheerio');
 let fs = require('fs');
 
-//https://comic.naver.com/webtoon/detail.nhn?titleId=651673&no=482
+// https://comic.naver.com/webtoon/detail.nhn?titleId=651673&no=482
+// titleId_no_??.jpg
 
-const downloadImage = (path, url) => {
+const downloadImage = (path, url, titleId, no, retryCount) => {
     request({
         url,
         headers: {
-            'referer': 'https://comic.naver.com/webtoon/detail.nhn?titleId=651673&no=482&weekday=wed'
+            'referer': `https://comic.naver.com/webtoon/detail.nhn?titleId=${titleId}&no=${no}&weekday=wed`
         },
         encoding: null
     }, function (error, response, body) {
-        console.error('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        // console.log('body:', body); // Print the HTML for the Google homepage.
+        if(error && --retryCount >= 0) {
+            console.log(`재시도 ${titleId} ${no} ${retryCount}`);
+            downloadImage(path, url, titleId, no, retryCount);
+            return;
+        }
 
-        fs.writeFile(path + '/' + (url.split('_IMAG')[1]) , body, null,  (err) => {
+        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+
+        fs.writeFile(path + '/' + `${titleId}_${no}_${(url.split('_IMAG01')[1])}`, body, null, (err) => {
             if (err) throw err;
             console.log('The file has been saved!');
         });
     });
 }
 
-request('https://comic.naver.com/webtoon/detail.nhn?titleId=651673&no=482&weekday=wed', function (error, response, body) {
-    const $ = cheerio.load(body);
-    for (let i = 1; i < $('.wt_viewer img').length; i++) {
-        downloadImage('download', $('.wt_viewer img')[i].attribs.src);
-    }
-});
+const getImageUrls = (titleId, no) => {
+    request('https://comic.naver.com/webtoon/detail.nhn?titleId=651673&no=482&weekday=wed', function (error, response, body) {
+        const $ = cheerio.load(body);
+        for (let i = 1; i < $('.wt_viewer img').length; i++) {
+            downloadImage('download', $('.wt_viewer img')[i].attribs.src, titleId, no);
+        }
+    });
+}
+
+for(let i=10, j=0; i<15; i++) {
+    setTimeout(() => {
+        getImageUrls(651673, i);
+    }, j * 1000);
+}
